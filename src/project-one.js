e61a741fe4c3a464fdfc799184ba444b90cf8db0
RileySpitznas <156121383+RileySpitznas@users.noreply.github.com>
2024-03-29 19:32:05 -0400

    check-in #2

--- src/project-one.js
+++ src/project-one.js
@@ -29,72 +29,76 @@ export class ProjectOne extends DDD {
       css`
 
       .container{
-            display: inline-flex;
-            flex-direction: row;
-
-          }
+          display: inline-flex;
+          flex-direction: row;
+        }
 
       :host {
-            display: block;
-            margin: 10px;
+         display: block;
+         margin: 10px;
             
-          }
+        }
 
       .mainBody {
-            height: 200px;
-            width: 200px;
-            max-width: 400px;
-            max-height: 400px;
-            display: flex;
-            flex-direction: column;
-            background-color: var(--ddd-theme-default-beaverBlue);
-            border: var(--ddd-border-lg);
-            color: var(--ddd-theme-default-keystoneYellow);
-            z-index:1;
-          }
+          height: 200px;
+          width: 200px;
+          max-width: 400px;
+          max-height: 400px;
+          display: flex;
+          flex-direction: column;
+          background-color: var(--ddd-theme-default-beaverBlue);
+          border: var(--ddd-border-lg);
+          color: var(--ddd-theme-default-keystoneYellow);
+          z-index:1;
+        }
+
+      .openPop {
+          width: 30%;
+        }
 
       .popUp {
-            display: flex;
-            flex-direction: column;
-            background-color: var(--ddd-theme-default-potential0);
-            border: var(--ddd-border-lg);
-            color: var(--ddd-theme-default-keystoneYellow);
-            position: relative;
-            right:200px; /* Initially hidden to the right */
-            transition: right 0.8s ease;
-            opacity: 0;
-            z-index:0;
-          }
+          display: flex;
+          flex-direction: column;
+          background-color: var(--ddd-theme-default-potential0);
+          border: var(--ddd-border-lg);
+          color: var(--ddd-theme-default-keystoneYellow);
+          position: relative;
+          right:200px; /* Initially hidden to the right */
+          transition: right 0.8s ease;
+          opacity: 0;
+          z-index:0;
+        }
 
       .sidebar{
-            max-width: 400px;
-            max-height: 400px;
-            height: 200px;
-            width: 200px;
-          }
+          max-width: 400px;
+          max-height: 400px;
+          height: 200px;
+          width: 200px;
+        }
 
       #rpg{
-            display: inline-flex;
-            margin: var(--ddd-spacing-3)
-          }
+          display: inline-flex;
+          margin: var(--ddd-spacing-3)
+        }
 
       #nameInput{
-            width:50%;
-            margin-top:16px;
-            display: inline-flex;
-            margin: var(--ddd-spacing-1)
-          }
+          width:50%;
+          margin-top:16px;
+          display: inline-flex;
+          margin: var(--ddd-spacing-1)
+        }
       
       .header{
-            background-color:var(--ddd-theme-default-keystoneYellow);
-            color: blue;
-            font: var(Roboto (ddd-font-primary) [--ddd-font-primary]);
-          }
+        background-color:var(--ddd-theme-default-keystoneYellow);
+        color: blue;
+        font: var(Roboto (ddd-font-primary) [--ddd-font-primary]);
+        }
 
       .rpgAdd{
-            width: 50%;
-            display: inline-flex;
-            margin: var(--ddd-spacing-6);
+        width: 50%;
+        display: inline-flex;
+        margin: var(--ddd-spacing-6);
+            
         }
 
       .headers{
@@ -107,14 +111,15 @@ export class ProjectOne extends DDD {
         }
 
       .rpgcharecter{
-          display:block;
+          display:flex;
+          
         }
 
       rpg-character{
-          max-width: 10vw;
-          max-height: 10vh;
-          padding: 8px;
-          margin: 8px;
+          max-width: 5vw;
+          max-height: 5vh;
+          padding: 28px;
+          margin: 28px;
         }
 
      .partyCont{
@@ -122,16 +127,46 @@ export class ProjectOne extends DDD {
         flex-direction:row;
         
         }
+     .buttonCont{
+            width:100%;
+            display:flex;
+            flex-direction:row;
+        }
+      .saveButton{
+            width: 30%;
+        }
+      #big-break{
+          width:10%;
+        } 
+        #small-break{
+           width:20%;
+        }
+
     `];
   }
 
   addToParty() {
-    const member = 
-    {
-      name: this.personName,
+      const randomNumber = globalThis.crypto.getRandomValues(new Uint32Array(1))[0];
+      const member = 
+        {
+          name: this.personName,
+          id: randomNumber,
+        }
+        this.party.push(member);
+        this.requestUpdate();
+  }
+
+  targetClicked(e) {
+    let value = e.target.id;
+
+    for(let i = 0; i < this.party.length; i++){
+
+        if(this.party[i].id == e.target.id){
+          this.party.splice(i, 1);
+          this.requestUpdate();
+          break;
+        }
     }
-    this.party.push(member);
-    this.requestUpdate();
   }
 
   openClick(){
@@ -142,34 +177,60 @@ export class ProjectOne extends DDD {
     this.personName = event.target.value;
   }
 
+
+//Confetti 
+  makeItRain() {
+   
+    import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        
+        setTimeout(() => {
+          
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
+
   main()
   {
     return html`
 
-    <div class ="container">
-        <div class="mainBody">
+<div class ="container">
+    
+    <div class="mainBody">
+      <confetti-container id="confetti">
         <div class="header">
-          place holder
+         Add Users
         </div>
-        <button class="openPop"  @click="${this.openClick}" > Add character</button>
-        <div class="partCont">
-        ${this.party.map((member) => html`
-        
-        <rpg-character id="${member.name}" seed = "${member.name}" > </rpg-character>
-        `)}
-        </div>
+          <div class="partCont">
+            ${this.party.map((member) => html`
+      
+              <rpg-character id="${member.id}" @click="${this.targetClicked}" seed = "${member.name}" > </rpg-character>
+              <div style= 'font-size:16px; text-align:center;' > ${member.name}</div>
+            `)}
+          </div>
+      <div class="buttonCont">
+        <div id="big-break"></div>
+          <button class="openPop"  @click="${this.openClick}" > Add character</button>
+            <div id="small-break"></div>
+              <button class="saveButton" @click = "${this.makeItRain}"> Save </button>
+        <div id="big-break"></div>
       </div>
-      <div class="sidebar">
-            <div class=popUp>
-              <div class=headers>    
-                </div>
-              <input id="nameInput" type="text" value=${this.personName} @input=${this.updateName}>
-            <rpg-character id="rpg" seed= ${this.personName}  > </rpg-character>
-            <button class= "rpgAdd" @click="${this.addToParty}" > Add to Party</button>
-        </div>
-        </div>
-        
-    </div>
+        </confetti-container>
+      </div>
+    
+    <div class="sidebar">
+    
+      <div class=popUp>
+      
+          <input id="nameInput" type="text" value=${this.personName} @input=${this.updateName}>
+          <rpg-character id="rpg" seed= ${this.personName}> </rpg-character>
+          
+          <button class= "rpgAdd" @click="${this.addToParty}" > Add to Party</button>
+      </div>
+    </div>     
+  </div>
     `;
   }
    
